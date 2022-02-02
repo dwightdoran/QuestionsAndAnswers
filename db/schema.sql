0428f3a94089ac789634fcbe419b50ca40484a15
Dwight Doran <dwight.d.doran@gmail.com>
2022-02-02 15:50:05 -0600

    ETL Complete // working on getting query time to around 50ms

--- db/schema.sql
+++ db/schema.sql
@@ -2,22 +2,17 @@
 
 -- CREATE DATABASE questions_and_answers;
 
-USE questions_and_answers;
+\c questions_and_answers;
 
-DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS answers;
 DROP TABLE IF EXISTS photos;
 
-CREATE TABLE products (
-  id BIGSERIAL PRIMARY KEY
-);
-
 CREATE TABLE questions (
   id BIGSERIAL PRIMARY KEY,
-  product_id BIGINT REFERENCES products(id),
+  product_id BIGINT,
   body VARCHAR(255),
-  date_written DATE,
+  date_written TEXT,
   asker_name VARCHAR(100),
   asker_email VARCHAR(100),
   reported BOOLEAN,
@@ -28,7 +23,7 @@ CREATE TABLE answers (
   id BIGSERIAL PRIMARY KEY,
   question_id BIGINT REFERENCES questions(id),
   body VARCHAR(255),
-  date_written DATE,
+  date_written TEXT,
   answerer_name VARCHAR(100),
   answerer_email VARCHAR(100),
   reported BOOLEAN,
@@ -40,3 +35,7 @@ CREATE TABLE photos (
   answer_id BIGINT REFERENCES answers(id),
   url TEXT
 );
+
+\copy questions FROM 'data/questions.csv' WITH (format csv, header);
+\copy answers FROM 'data/answers.csv' WITH (format csv, header);
+\copy photos FROM 'data/answers_photos.csv' WITH (format csv, header);
