3281126ce3811b45727f55c040eb15150a94dd19
Dwight Doran <dwight.d.doran@gmail.com>
2022-02-02 10:41:49 -0600

    schema design mirrored on CSV file headers

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,43 @@
 -- set up schema
+
+CREATE DATABASE questionsAndAnswers;
+
+USE questionsAndAnswers;
+
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS photos;
+
+CREATE TABLE products (
+  id BIGSERIAL PRIMARY KEY
+)
+
+CREATE TABLE questions (
+  id BIGSERIAL PRIMARY KEY,
+  product_id BIGINT REFERENCES products(id),
+  body VARCHAR(255),
+  date_written DATE,
+  asker_name VARCHAR(100),
+  asker_email VARCHAR(100),
+  reported BOOLEAN,
+  helpfulness INTEGER
+)
+
+CREATE TABLE answers (
+  id BIGSERIAL PRIMARY KEY,
+  question_id BIGINT REFERENCES questions(id),
+  body VARCHAR(255),
+  date_written DATE,
+  answerer_name VARCHAR(100),
+  answerer_email VARCHAR(100),
+  reported BOOLEAN,
+  helpfulness INTEGER
+)
+
+CREATE TABLE photos (
+  id BIGSERIAL PRIMARY KEY,
+  answer_id BIGINT REFERENCES answers(id),
+  url TEXT
+)
+-- researching foreign keys and schema design first
