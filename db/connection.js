a8a818c84624f22690979937a335fc9401de9dbd
Dwight Doran <dwight.d.doran@gmail.com>
2022-02-07 10:35:53 -0600

    added .env with DB login data

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,30 @@
-// connect to db
+// // connect to db
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'dwightdoran',
+//   port: 5432,
+//   // dot env to hide password
+//   password: '',
+//   database: 'questions_and_answers'
+//   // max: set max number of connections?
+//   // connectionTimeoutMillis: how long to wait if every connection is busy
+//   // idleTimeoutMillis: how long to wait until the connection attempt is dropped (its using memory so not specifying is bad)
+// });
+
+// pool.connect();
+
+// pool.query(`SELECT questions_id, question_body, question_date_written,
+// asker_name, question_reported, question_helpfulness,
+// answers_id, answer_body, answer_date_written, answerer_name, answer_reported,
+// answer_helpfulness
+// FROM questions, answers
+// WHERE questions.questions_id = answers.question_id
+// AND product_id = 150000;`, (err, res) => {
+//   if (err) {
+//     console.log('error with query :', err)
+//   } else {
+//     console.table(res.rows)
+//   }
+// })
--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 // routes go in here
 const express = require('express');
+const { Pool } = require('pg');
+
+require('dotenv/config');
 
 const app = express();
 
 app.use(express.json())
+
+const pool = new Pool({
+  host: process.env.DB_Host,
+  user: process.env.DB_User,
+  port: process.env.DB_Port,
+  // dot env to hide password
+  password: process.env.DB_Password,
+  database: 'questions_and_answers'
+  // max: set max number of connections?
+  // connectionTimeoutMillis: how long to wait if every connection is busy
+  // idleTimeoutMillis: how long to wait until the connection attempt is dropped (its using memory so not specifying is bad)
+});
+
+pool.connect();
 
 app.get('/', async (req, res) => {
   res.status(200).json({});
