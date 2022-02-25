a3523805b9eae2429306ef9a38f27fd7c1191028
Dwight Doran <dwight.d.doran@gmail.com>
2022-02-24 22:35:42 -0600

    local stress testing with k6

--- load-test/script.js
+++ load-test/script.js
@@ -1,7 +1,23 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  stages: [
+    // { duration: '10s', target: 10},
+    // { duration: '10s', target: 100}
+    // { duration: '30s', target: 1000}
+    // { duration: '30s', target: 1200}
+    { duration: '3m', target: 1350}
+    // { duration: '10s', target: 1400}
+    // { duration: '10s', target: 1500}
+    // { duration: '10s', target: 1600}
+  ]
+}
+
 export default function () {
-  http.get('http://localhost:3000/qa/questions?product_id=64620');
+  http.get(`http://localhost:3000/qa/questions?product_id=${Math.floor(Math.random() * (1000011 - 900001) + 900001)} `);
+  // http.get(`http://localhost:3000/qa/questions?product_id=${id}`);
+
   sleep(1);
 }
+
